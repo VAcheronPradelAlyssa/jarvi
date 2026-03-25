a5a0b98d7182d73113a327eb9f1029d1db4c5a89
Alyssa Vacheron <vacheron.alyssa@aol.com>
2026-03-25 14:21:06 +0100

    ajout des routes de navigation pour la page d'accueil

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Home } from './components/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{ path: '', pathMatch: 'full', redirectTo: 'home' },
+	{ path: 'home', component: Home },
+	{ path: '**', redirectTo: 'home' }
+];
+  
