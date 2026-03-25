7e3387bbb101f0ad93a7ce39f4069dbd4331ae2c
Alyssa Vacheron <vacheron.alyssa@aol.com>
2026-03-25 14:10:51 +0100

    création components accueil

---
+++ src/app/components/acceuil/acceuil.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-acceuil',
+  imports: [],
+  templateUrl: './acceuil.html',
+  styleUrl: './acceuil.scss',
+})
+export class Acceuil {}
