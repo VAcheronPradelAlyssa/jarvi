4456df59c6a49a177a8e2cd2b7323a7c777462fc
Alyssa Vacheron <vacheron.alyssa@aol.com>
2026-03-25 16:28:09 +0100

    ajout du composant Logistics et mise à jour des routes : ajout de la redirection vers le composant Logistics depuis la page d'accueil

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
-export class Home {}
+export class Home {
+  constructor(private router: Router) {}
+
+  goToLogistics() {
+    this.router.navigateByUrl('/logistics');
+  }
+}
