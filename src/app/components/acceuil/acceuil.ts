4a8b177fba15caac55702f53382366d53f8509c8
Alyssa Vacheron <vacheron.alyssa@aol.com>
2026-03-25 14:13:47 +0100

    changement erreur nom

--- src/app/components/acceuil/acceuil.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-acceuil',
-  imports: [],
-  templateUrl: './acceuil.html',
-  styleUrl: './acceuil.scss',
-})
-export class Acceuil {}
