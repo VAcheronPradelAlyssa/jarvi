05fb8ce6b962f29dee21060272ab50faab4ae0d7
Alyssa Vacheron <vacheron.alyssa@aol.com>
2026-03-26 09:44:01 +0100

    ajout de la fonctionnalité de relance des vidéos sur la page Logistics : ajout de boutons pour relancer les vidéos et mise à jour des styles

--- src/app/components/logistics/logistics.ts
+++ src/app/components/logistics/logistics.ts
@@ -1,15 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-logistics',
   standalone: true,
-    imports: [NgIf],
-    templateUrl: './logistics.html',
-    styleUrls: ['./logistics.scss'],
+  imports: [NgIf],
+  templateUrl: './logistics.html',
+  styleUrls: ['./logistics.scss'],
 })
-  export class Logistics {
+export class Logistics implements AfterViewInit {
   zoomed = false;
+
+  @ViewChild('voitureVideo') voitureVideo?: ElementRef<HTMLVideoElement>;
+  @ViewChild('trainVideo') trainVideo?: ElementRef<HTMLVideoElement>;
+
+
+  ngAfterViewInit() {
+    // Lance la vidéo voiture si possible
+    if (this.voitureVideo?.nativeElement) {
+      this.voitureVideo.nativeElement.play().catch(() => {});
+    }
+    // Lance la vidéo train si possible
+    if (this.trainVideo?.nativeElement) {
+      this.trainVideo.nativeElement.play().catch(() => {});
+    }
+  }
+
+  replayVideo(type: 'voiture' | 'train') {
+    let video: HTMLVideoElement | undefined;
+    if (type === 'voiture') {
+      video = this.voitureVideo?.nativeElement;
+    } else if (type === 'train') {
+      video = this.trainVideo?.nativeElement;
+    }
+    if (video) {
+      video.currentTime = 0;
+      video.play().catch(() => {});
+    }
+  }
 
   toggleZoom() {
     this.zoomed = !this.zoomed;
