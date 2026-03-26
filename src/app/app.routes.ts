83fb723bfd31c67075a97a8faadbd9bbfedc6e29
Alyssa Vacheron <vacheron.alyssa@aol.com>
2026-03-26 12:20:26 +0100

    ajout de la route pour le composant Projects et mise à jour du contenu de la page Projects

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,14 @@
+
 import { Routes } from '@angular/router';
 import { Home } from './components/home/home';
-import { Logistics } from './components/logistics/logistics';
+import { Logistics} from './components/logistics/logistics';
+import { Projects } from './components/projects/projects';
 
 export const routes: Routes = [
 	{ path: '', pathMatch: 'full', redirectTo: 'home' },
 	{ path: 'home', component: Home },
 	{ path: 'logistics', component: Logistics },
+	{ path: 'projects', component: Projects },
 	{ path: '**', redirectTo: 'home' }
 ];
   
